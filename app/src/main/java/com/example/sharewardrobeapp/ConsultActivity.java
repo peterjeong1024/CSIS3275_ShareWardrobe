0795c5bb6553643cd664bba9708ba272da03b515
patri <patricia.isnandar@yahoo.com>
2022-07-31 20:38:47 -0700

    test consult 2

--- app/src/main/java/com/example/sharewardrobeapp/ConsultActivity.java
+++ app/src/main/java/com/example/sharewardrobeapp/ConsultActivity.java
@@ -3,9 +3,12 @@ package com.example.sharewardrobeapp;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.sharewardrobeapp.consultations.ConsultationsRecyclerAdapter;
 import com.example.sharewardrobeapp.consultations.ConsultationsViewModel;
 import com.example.sharewardrobeapp.objects.Consultations;
 import com.example.sharewardrobeapp.objects.FashionItem;
@@ -15,6 +18,11 @@ import com.example.sharewardrobeapp.util.UseLog;
 import java.util.ArrayList;
 
 public class ConsultActivity extends BasementActivity {
+
+    RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+    RecyclerView.Adapter adapter;
+
 
     private ConsultationsViewModel mViewModel;
     private ArrayList<Consultations> mConsultationsItems;
@@ -34,6 +42,11 @@ public class ConsultActivity extends BasementActivity {
                 // data(fashionItems) is brought here  automatically and asynchronously
                 mConsultationsItems = items;
                 displayScreen(items);
+
+                recyclerView = findViewById(R.id.consultRecyclerView);
+                recyclerView.setLayoutManager(layoutManager);
+                adapter = new ConsultationsRecyclerAdapter();
+                recyclerView.setAdapter(adapter);
             }
         });
     }
